5ecf300d5e1ce1e17ad80402a4a485999486eca9
Connor <connor.cs@protonmail.com>
2022-12-31 17:19:06 -0500

    implement routes and nav bar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 
 import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components/';
+import './App.css'
+
 
 export default function App() {
     return (
@@ -14,22 +16,32 @@ export default function App() {
                 <Layout>
                     <div className='routes'>
                         <Routes>
-                            <Route path='/' element={<Homepage />}/>
-                                
-                            <Route path='/exchanges' element={<Exchanges />}/>
-                    
+                            <Route path='/' element={<Homepage />} />
+
+                            <Route path='/exchanges' element={<Exchanges />} />
+
                             <Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
-                                
-                            <Route path='/crypto/:coinId' element={<CryptoDetails />}/>
-                                
-                            <Route path='/news' element={<News />}/>
-                            
+
+                            <Route path='/crypto/:coinId' element={<CryptoDetails />} />
+
+                            <Route path='/news' element={<News />} />
+
                         </Routes>
                     </div>
                 </Layout>
-            </div>
-            <div className='footer'>
 
+                <div className='footer' >
+                    <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>
+                        Cryptoverse <br />
+                        All rights reserved
+                    </Typography.Title>
+                    <Space >
+                        <Link to='/'>Home</Link>
+                        <Link to='/exchanges'>Exchanges</Link>
+                        <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
+                        <Link to='/news'>News</Link>
+                    </Space>
+                </div>
             </div>
         </div>
     )
